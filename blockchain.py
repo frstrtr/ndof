38f93f74bd162035083df4140065449a1993d5d5
frstrtr <frstrtr@gmail.com>
2019-08-23 21:12:39 +0300

    First try

--- blockchain.py
+++ blockchain.py
@@ -1,2 +1,16 @@
+
+"""
 genesis = 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks'
 blockchain = [1,2,3,4,5,6,7,8,9,10]
+print(genesis)
+"""
+
+blockchain = [[1]]
+
+def add_value():
+    blockchain.append([blockchain[-1],[5.3]])
+    print(blockchain)
+
+add_value()
+add_value()
+add_value()
